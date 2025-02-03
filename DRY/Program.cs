e93571daa66b1ee54a114b8a2ab76a350684b019
AlvelyBR <alvelybencosmeramos@gmail.com>
2025-02-02 22:55:34 -0400

    DRY

--- DRY/Program.cs
+++ DRY/Program.cs
@@ -1,4 +1,52 @@
 ﻿namespace DRY
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var salario = new Salario();
 
+            Console.WriteLine("Seleccione el tipo de empleado: \n1.Tiempo completo \n2.Por hora");
+            string tipoEmpleado = Console.ReadLine();
+
+            decimal totalSalary = 0;
+
+            switch (tipoEmpleado)
+            {
+                
+                case "1":
+                    try
+                    {
+                        Console.WriteLine("Escriba el sueldo base: ");
+                        decimal salarioBase = decimal.Parse(Console.ReadLine());
+                        totalSalary = salario.CalForFullTime(salarioBase);
+                    }
+                    catch (FormatException) { Console.WriteLine("Entrada no valida, ingrese un número."); }
+
+                    break;
+
+                case "2":
+                    try
+                    {
+                    Console.WriteLine("Escriba el sueldo por hora: ");
+                    decimal hoursRate = decimal.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Escriba las horas trabajadas: ");
+                    int hoursWorked = int.Parse(Console.ReadLine());
+                    totalSalary = salario.CalForPartTime(hoursRate, hoursWorked);
+                    }
+                    catch (FormatException) { Console.WriteLine("Entrada no valida, ingrese un número."); }
+                    break;
+
+                
+
+                default:
+                    Console.WriteLine("Tipo de Empleado no existe");
+                    return;
+
+            }
+
+            Console.WriteLine($"Salario total con impuestos y bonus incluidos: {totalSalary:C}");
+        }
+    }
 }
---
+++ DRY/Salario.cs
@@ -0,0 +1,39 @@
+﻿
+namespace DRY
+{
+    public class Salario
+    {
+        private const decimal Tax = 0.18m;
+        private const decimal Bonus = 0.05m;
+
+        private decimal CalTax(decimal monto)
+        {
+            return monto * Tax;
+        }
+
+        private decimal CalBonus(decimal monto)
+        {
+            return monto * Bonus;
+        }
+
+        private decimal SalarioNeto(decimal salarioBruto)
+        {
+            decimal tax = CalTax(salarioBruto);
+            decimal bonus = CalBonus(salarioBruto);
+
+            return (salarioBruto - tax) + bonus;
+        }
+
+        public decimal CalForFullTime(decimal salarioBase)
+        {
+            return SalarioNeto(salarioBase);
+
+        }
+
+        public decimal CalForPartTime(decimal hoursRate, decimal hoursWorked)
+        {
+            decimal salarioBruto = hoursRate * hoursWorked;
+            return SalarioNeto(salarioBruto);
+        }
+    }
+}
