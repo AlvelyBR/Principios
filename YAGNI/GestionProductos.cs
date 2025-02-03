52c7e1880e2bdb10be63a83cea6e6eb4b49ea324
AlvelyBR <alvelybencosmeramos@gmail.com>
2025-02-03 00:26:35 -0400

    YAGNI

---
+++ YAGNI/GestionProductos.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace YAGNI
+{
+    public class GestionProductos
+    {
+        public void AddProduct(string name, decimal price)
+        {
+            Console.WriteLine($"Producto {name} agregado correctamente. " );
+        }
+
+        public void DeteleProducto(int id)
+        {
+            Console.WriteLine($"Producto de ID {id} eliminado. ");
+        }
+    }
+}
---
+++ YAGNI/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace YAGNI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GestionProductos producto = new GestionProductos();
+
+            Console.WriteLine("Selecione una opcion:\n \n1.Agregar Producto\n2.Eliminar producto");
+
+            int opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    try
+                    {
+                        Console.WriteLine("Escriba el nombre del producto");
+                        string name = Console.ReadLine();
+
+                        Console.WriteLine("ingrese el precio del producto ");
+                        decimal price = decimal.Parse(Console.ReadLine());
+                   
+
+                    producto.AddProduct(name, price);
+                    }
+                    catch (FormatException)
+                    { Console.WriteLine("Entrada no valida. "); }
+
+                    break;
+
+                    case 2:
+                    try
+                    {
+                        Console.WriteLine("Escribe el ID del producto que va a eliminar. ");
+                        int id = int.Parse(Console.ReadLine());
+                        producto.DeteleProducto(id);
+
+                    }
+                    catch (FormatException)
+                    { Console.WriteLine("Entrada no valida. "); }
+
+                    break;
+
+                default:
+                    Console.WriteLine("Opción no valida. ");
+                    break;
+            }
+        }
+
+    }
+}
