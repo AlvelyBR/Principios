4c70fe2a1f17b80d308d91fbb9d481139ce55f4d
AlvelyBR <alvelybencosmeramos@gmail.com>
2025-02-02 21:25:01 -0400

    a

---
+++ KISS/Factura.cs
@@ -0,0 +1,30 @@
+﻿namespace KISS
+{
+    public class Factura
+    {
+        private const decimal propinaFija = 0.10m;
+
+        private decimal SumarPrecios(string precios)
+        {
+
+            return precios.Split(',').Select(decimal.Parse).Sum();
+
+        }
+
+        private decimal CalPropina(decimal total, decimal propinaAPagar)
+        {
+
+            return total * propinaAPagar;
+
+        }
+
+        public decimal ProcesodePago(string precios, bool propinaPersonalizada = false, decimal propinaAPagar = propinaFija )
+        {
+            var total = SumarPrecios(precios);
+            var propina = CalPropina(total, propinaAPagar);
+
+            return total + propina;
+
+        }
+    }
+}
--- KISS/Program.cs
+++ KISS/Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Hola")
+﻿namespace KISS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factura = new Factura();
+
+            try
+            {
+
+                Console.WriteLine("Ingrese los precios de los platos (separados por comas):");
+                var precios = Console.ReadLine();
+
+                Console.WriteLine("¿Desea agregar una propina personalizada? (s/n):");
+                var respuesta = Console.ReadLine().ToLower();
+
+                decimal totalFinal;
+
+
+
+                if (respuesta == "s")
+                {
+
+                    Console.Write("Ingrese el porcentaje de propina: ");
+                    var porcentaje = decimal.Parse(Console.ReadLine()) / 100;
+
+                    totalFinal = factura.ProcesodePago(precios, true, porcentaje);
+                }
+                else
+                {
+                    totalFinal = factura.ProcesodePago(precios);
+                }
+
+                Console.WriteLine($"Tu cuenta final es de: {totalFinal:C}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error {ex.Message}");
+            }
+
+        }
+
+    }
+
+}
